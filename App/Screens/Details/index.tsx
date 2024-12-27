e36f55e23553ef5cd0b7c74c1f7d9e0cb52b8af5
YadwinderSingh48 <=>
2024-12-27 23:33:24 +0530

    Entering Layout animation added

--- App/Screens/Details/index.tsx
+++ App/Screens/Details/index.tsx
@@ -1,10 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Animated, { FadeInLeft } from 'react-native-reanimated';
 
-const Details = () => {
+const Details = ({route}: any) => {
+  const {item} = route.params;
   return (
     <View>
-      <Text>Details</Text>
+      {/* Layout Animations Entering and Exiting   */}
+      {/*  For More check the docs here https://docs.swmansion.com/react-native-reanimated/docs/layout-animations/entering-exiting-animations */}
+      <Animated.Text entering={FadeInLeft.duration(500).delay(400)} style={{fontSize:18, fontWeight:'bold', padding:16}}>{item?.title}</Animated.Text>
+      <Animated.Text entering={FadeInLeft.duration(500).delay(600)} style={{fontSize:18, padding:16}}>{item?.description}</Animated.Text>
+      {/* <Animated.Text style={{fontSize:18, padding:16}}>{item?.title}</Animated.Text> */}
     </View>
   )
 }
